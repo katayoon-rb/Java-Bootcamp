1c3bf9a723bc21f7d83858f997881662e108a9df
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-11-20 21:06:23 +0330

    Add files via upload

---
+++ 01/1 Average.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float num1 = scanner.nextFloat();
+        float num2 = scanner.nextFloat();
+
+        System.out.println((num1 + num2) / 2);
+    }
+}
